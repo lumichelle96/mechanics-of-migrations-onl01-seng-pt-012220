75dce270c89d42f2830b4087f773c0ee59cc6e5b
Michelle Lu <lu.michelle96@icloud.com>
2020-04-28 00:25:47 +0000

    Done.

---
+++ artist.rb
@@ -0,0 +1,14 @@
+class Artist < ActiveRecord::Base
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      length INTEGER
+    )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+  end
+end
