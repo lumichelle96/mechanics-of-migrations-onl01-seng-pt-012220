673b73c9a9761bef185d60f43342485b48f4d880
Michelle Lu <lu.michelle96@icloud.com>
2020-04-28 00:03:39 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,17 @@
-# db/migrate/01_create_artists.rb
-
 class CreateArtists < ActiveRecord::Migration[5.2]
+
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      title TEXT, 
+      length INTEGER
+      )
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
   def change
     create_table :artists do |t|
       t.string :name
@@ -9,4 +20,5 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       t.string :hometown
     end
   end
-end
+
+end 
